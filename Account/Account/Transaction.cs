12a79f11e855d8c8afe48ce7d5844e51b3653f27
Feekra Baset Baishakhi <feekrabaset@gmail.com>
2021-03-21 14:02:22 +0600

    Transaction

---
+++ Account/Account/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class Transaction
+
+    {
+        Account sender;
+        Account receiver;
+        private int amount;
+        private String additionalInfo;
+
+
+    }
+}
